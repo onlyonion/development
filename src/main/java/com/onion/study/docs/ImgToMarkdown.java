ccaed30bca058760e4aff302eba2a63baf4f630f
lijicong <lijicong@wdai.com>
2018-06-14 17:55:20 +0800

    update

--- src/main/java/com/onion/study/docs/ImgToMarkdown.java
+++ src/main/java/com/onion/study/docs/ImgToMarkdown.java
@@ -1,6 +1,10 @@
 package com.onion.study.docs;
 
 public class ImgToMarkdown {
+    /**
+     * 将img文件夹下的图像在父路径生成 99-img.md 文件，方便阅读
+     * @param args
+     */
     public static void main(String[] args) {
         MarkdownUtils.generate();
     }
